bf40cdf2c75bd194ebc1cf7b6e5b3586c42d369e
Albin Baby <albinbaby150@gmail.com>
2022-06-10 12:47:39 +0200

    random card generation

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+"""creating a function fro random card generation"""
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    random_card = random.choice(cards)
+    return random_card
