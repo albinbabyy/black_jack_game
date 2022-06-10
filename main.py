534026850153b9957be8f388b3f6f84fd6b182ee
Albin Baby <albinbaby150@gmail.com>
2022-06-10 13:11:58 +0200

    project done

--- main.py
+++ main.py
@@ -20,11 +20,29 @@ def calculate_score(cards):
     return sum(cards)
 
 
+def compare(user_score, computer_score):
+
+    if user_score == computer_score:
+        return "draw"
+    elif computer_score == 0:
+        return "you lose, opponent has blackjack"
+    elif user_score == 0:
+        return "you win"
+    elif user_score > 21:
+        return "you went over, you lose"
+    elif computer_score > 21:
+        return "opponent went over, you win"
+    elif user_score > computer_score:
+        return "you win"
+    else:
+        return "you lost"
+
+
 """creating empty list for us and computer to store the card values"""
-
-
 user_card = []
 computer_card = []
+is_game_over = True
+
 for _ in range(2):
     new_card = deal_card()
     user_card.append(new_card)
@@ -35,8 +53,8 @@ while not is_game_over:
     user_score = calculate_score(user_card)
     computer_score = calculate_score(computer_card)
 
-    print(f"your cards: {user_card}, current score : {user_score}")
-    print(f"computer cards: {computer_card[0]}")
+    # print(f"your cards: {user_card}, current score : {user_score}")
+    # print(f"computer cards: {computer_card[0]}")
 
     if user_score == 0 or computer_score == 0 or user_score > 21:
         is_game_over = True
@@ -50,3 +68,8 @@ while not is_game_over:
 while computer_score != 0 and computer_score < 17:
     computer_card.append(deal_card())
     computer_score = calculate_score(computer_card)
+
+print(f" your final hadnd : {user_card}, final_score : {user_score}")
+print(f" computer final hand: {computer_card}, computer_score: {computer_score}")
+print(compare(user_score, computer_score))
+
