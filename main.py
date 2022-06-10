8880638961bf3adf954b9c476705c5deaf8a1833
Albin Baby <albinbaby150@gmail.com>
2022-06-10 13:00:59 +0200

    conditions defined check to win or lose and repeatition feature added

--- main.py
+++ main.py
@@ -30,8 +30,23 @@ for _ in range(2):
     user_card.append(new_card)
     computer_card.append(new_card)
 
+while not is_game_over:
+
     user_score = calculate_score(user_card)
     computer_score = calculate_score(computer_card)
 
-print(f"your cards: {user_card}, current score : {user_score}")
-print(f"computer cards: {computer_card[0]}")
+    print(f"your cards: {user_card}, current score : {user_score}")
+    print(f"computer cards: {computer_card[0]}")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get a another card, type 'n' to pass: ")
+        if user_should_deal == "y":
+            user_card.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_card.append(deal_card())
+    computer_score = calculate_score(computer_card)
